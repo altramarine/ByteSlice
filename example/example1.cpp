e09f62ffdacebb6d4b50587aa4ef8bca5ba2371f
altramarine <jerome_wei@outlook.com>
2024-09-20 16:39:18 +0800

    Update example1.cpp

--- example/example1.cpp
+++ example/example1.cpp
@@ -23,12 +23,13 @@ std::map<std::string, ColumnType> ctypeMap = {
 
 typedef struct {
     ColumnType  coltype = ColumnType::kByteSlicePadRight;
-    size_t      size    = 16*1024*1024;
-    size_t      nbits   = 12;
+    size_t      size    = 1000000;
+    size_t      nbits   = 32;
     double      selectivity = 0.1;
     size_t      repeat  = 3;
+    std::string ingestion;
+    std::string query;
 } arg_t;
-
 
 void parse_arg(arg_t &arg, int &argc, char** &argv);
 void print_arg(const arg_t& arg);
@@ -36,49 +37,69 @@ void print_arg(const arg_t& arg);
 int main(int argc, char* argv[]){
     arg_t arg;
     parse_arg(arg, argc, argv);
-    
+
+    FILE * fingestion = fopen(arg.ingestion.c_str(), "r");
+    FILE * fquery = fopen(arg.query.c_str(), "r");
+
+
     std::cout << "[INFO ] Creating column ..." << std::endl;
     Column* column = new Column(arg.coltype, arg.nbits, arg.size);
     std::cout << "[INFO ] Creating bit vector ..." << std::endl;
-    BitVector* bitvector = new BitVector(column);
+    BitVector* bitvectorL = new BitVector(column);
+    BitVector* bitvectorR = new BitVector(column);
     
     std::cout << "[INFO ] Populating column with random values ..." << std::endl;
-    auto dice = std::bind(std::uniform_int_distribution<WordUnit>(
-                            std::numeric_limits<WordUnit>::min(),
-                            std::numeric_limits<WordUnit>::max()),
-                            std::default_random_engine(std::time(0)));
-    WordUnit mask = (1ULL << arg.nbits) - 1;
+    // auto dice = std::bind(std::uniform_int_distribution<WordUnit>(
+    //                         std::numeric_limits<WordUnit>::min(),
+    //                         std::numeric_limits<WordUnit>::max()),
+    //                         std::default_random_engine(std::time(0)));
+    // WordUnit mask = (1ULL << arg.nbits) - 1;
+
+    std::cout << arg.ingestion << std::endl;;
     for(size_t i=0; i < arg.size; i++){
-        column->SetTuple(i, dice() & mask);
+        int value;
+        fscanf(fingestion, "%d", &value);
+        column->SetTuple(i, value);
     }
-    
+
     std::cout << "[INFO ] omp_max_threads = " << omp_get_max_threads() << std::endl;
     std::cout << "[INFO ] Executing scan ..." << std::endl;
-    HybridTimer t1;
-    t1.Start();
-    for(size_t r = 0; r < arg.repeat; r++){
+    int ql, qr;
+    double tot_time = 0;
+
+    std::cout << "query file is " << arg.query << std::endl;
+    for(;fscanf(fquery, "%d %d", &ql, &qr) != EOF;){
+        HybridTimer t1;
+        t1.Start();
         column->Scan(Comparator::kLess,
-                    static_cast<WordUnit>(mask*arg.selectivity),
-                    bitvector,
+                    static_cast<WordUnit>(ql),
+                    bitvectorL,
                     Bitwise::kSet);
+                    
+        column->Scan(Comparator::kLess,
+                    static_cast<WordUnit>(qr),
+                    bitvectorR,
+                    Bitwise::kSet);
+        t1.Stop();
+        std::cout << bitvectorR->CountOnes() - bitvectorL->CountOnes() << std::endl;
+        tot_time += t1.GetSeconds();
     }
-    t1.Stop();
-    
-    std::cout << "Wall time (sec), CPU cost (cycle/value)" << std::endl;
-    std::cout << t1.GetSeconds()/arg.repeat << ", "
-                << double(t1.GetNumCycles()/arg.repeat)/arg.size
-                << std::endl;
+
                 
+    std::cout << "tatal time (sec) : ";
+    std::cout << tot_time 
+                    << std::endl;
     std::cout << "[INFO ] Releasing memory ..." << std::endl;
     delete column;
-    delete bitvector;
+    delete bitvectorL;
+    delete bitvectorR;
 }
 
 
 void parse_arg(arg_t &arg, int &argc, char** &argv){
     int c;
     std::string s;
-    while((c = getopt(argc, argv, "t:s:b:y:r:h")) != -1){
+    while((c = getopt(argc, argv, "t:s:b:y:r:i:q:h")) != -1){
         switch(c){
             case 'h':
                 std::cout << "Usage: " << argv[0]
@@ -87,6 +108,8 @@ void parse_arg(arg_t &arg, int &argc, char** &argv){
                 << " [-b <bit width>]"
                 << " [-y <selectivity>]"
                 << " [-r <repeat>]"
+                << " [-i <ingetsion filedir>] "
+                << " [-q <query filedir>] "
                 << std::endl;
                 exit(0);
             case 't':
@@ -111,6 +134,11 @@ void parse_arg(arg_t &arg, int &argc, char** &argv){
             case 'r':
                 arg.repeat = atoi(optarg);
                 break;
+            case 'i':
+                arg.ingestion = std::string(optarg);
+                break;
+            case 'q':
+                arg.query = std::string(optarg);
         }
     }
     
